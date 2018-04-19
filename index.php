dabf18004bc6499edb53444f2017c64997babb44
opiner isang <opinerdan@gmail.com>
2018-04-19 15:16:03 +0100

    stage1 task added

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>stage1</title>
+<link rel="stylesheet" href="index.css" type="text/css" />
+
+
+
+
+</head>
+
+<body>
+<div class="nav">
+  <div class="link"><a href="#">Login</a></div>
+  </div>
+         
+
+<div class="welcome">
+
+   <h1>Hello World! World of PHP</h1>
+   <h3>Real Time: <?php $tm=date("h:i:s:a");
+		 echo $tm; ?></h3>
+         
+
+ 
+
+
+
+</body>
+</html>
